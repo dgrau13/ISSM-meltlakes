22db524976c621f25358b80668a324a89673a760
dgrau13 <dgrau7@gatech.edu>
2026-08-06 19:50:10 -0400

    Updating CalvingCrevasseDepth.py for parameterization parameters from Grau et. al., 2025.

--- src/m/classes/calvingcrevassedepth.py
+++ src/m/classes/calvingcrevassedepth.py
@@ -14,7 +14,11 @@ class calvingcrevassedepth(object):
     def __init__(self):  # {{{
         self.crevasse_opening_stress = 1
         self.crevasse_threshold      = 1.
+	self.surface_hydrology_type = 1.
         self.water_height            = 0.
+	self.hurst = 0.
+	self.sigma = 0.
+	self.melt_supply = 0.
 
         #self.setdefaultparameters() # Uncomment if/when setdefaultparameters is used
     # }}}
@@ -23,6 +27,9 @@ class calvingcrevassedepth(object):
         s += '{}\n'.format(fielddisplay(self, 'crevasse_opening_stress', '0: stress only in the ice-flow direction, 1: max principal'))
         s += '{}\n'.format(fielddisplay(self, 'crevasse_threshold', 'ratio of full thickness to calve (e.g. 0.75 is for 75% of the total ice thickness)'))
         s += '{}\n'.format(fielddisplay(self, 'water_height', 'water height in the crevasse [m]'))
+	s += '{}\n'.format(fielddisplay(self, 'hurst', 'Hurst exponent'))
+	s += '{}\n'.format(fielddisplay(self, 'sigma', 'Roughness Amplitude [m]'))
+	s += '{}\n'.format(fielddisplay(self, 'melt_supply', 'Amount of supply meltwater able to be distributed across the surface [m]'))
         return s
     # }}}
     def setdefaultparameters(self):  # {{{
@@ -38,7 +45,17 @@ class calvingcrevassedepth(object):
 
         md = checkfield(md, 'fieldname', 'calving.crevasse_opening_stress', 'numel', [1], 'values', [0,1])
         md = checkfield(md, 'fieldname', 'calving.crevasse_threshold', 'numel', [1], '>', 0., '<=', 1.)
-        md = checkfield(md, 'fieldname', 'calving.water_height', 'NaN', 1, 'Inf', 1, 'timeseries', 1, '>=', 0) 
+	md = checkfield(md, 'fieldname', 'calving.surface_hydrology_type', 'numel', [1], 'values', [0,1,2])
+	if surface_hydrology_type ==0:
+		md = checkfield(md, 'fieldname', 'calving.water_height', 'NaN', 1, 'Inf', 1, 'timeseries', 1, '>=', 0)
+	if surface_hydrology_type ==1:
+		md = checkfield(md, 'fieldname', 'calving.hurst', 'NaN', 1, 'size', [md.md.mesh.numberofvertices 1],'>=',0,'<=',1)
+		md = checkfield(md, 'fieldname', 'calving.sigma', 'NaN', 1, 'size', [md.mesh.numberofvertices 1],'>=',0)
+		md = checkfield(md, 'fieldname', 'calving.melt_supply', 'NaN', 1, 'Inf', 1, 'timeseries', 1, '>=' 0)
+	if surface_hydrology_type ==2:
+                md = checkfield(md, 'fieldname', 'calving.hurst', 'NaN', 1, 'size', [md.md.mesh.numberofvertices 1],'>=',0,'<=',1)
+                md = checkfield(md, 'fieldname', 'calving.sigma', 'NaN', 1, 'size', [md.mesh.numberofvertices 1],'>=',0)
+                md = checkfield(md, 'fieldname', 'calving.melt_supply', 'NaN', 1, 'Inf', 1, 'timeseries', 1, '>=' 0)
 
         return md
     # }}}
@@ -47,5 +64,15 @@ class calvingcrevassedepth(object):
         WriteData(fid, prefix, 'name', 'md.calving.law', 'data', 6, 'format', 'Integer')
         WriteData(fid, prefix, 'object', self, 'fieldname', 'crevasse_opening_stress', 'format', 'Integer')
         WriteData(fid, prefix, 'object', self, 'fieldname', 'crevasse_threshold', 'format', 'Double')
-        WriteData(fid, prefix, 'object', self, 'fieldname', 'water_height', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+	WriteData(fid, prefix, 'object', self, 'fieldname', 'surface_hydrology_type', 'format', 'Integer')
+	if surface_hydrology_type ==0:
+        	WriteData(fid, prefix, 'object', self, 'fieldname', 'water_height', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+	if surface_hydrology_type ==1:
+		WriteData(fid, prefix, 'object', self, 'fieldname', 'hurst', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+		WriteData(fid, prefix, 'object', self, 'fieldname', 'sigma', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+		WriteData(fid, prefix, 'object', self, 'fieldname', 'melt_supply', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+	if surface_hydrology_type ==2:
+                WriteData(fid, prefix, 'object', self, 'fieldname', 'hurst', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+                WriteData(fid, prefix, 'object', self, 'fieldname', 'sigma', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
+                WriteData(fid, prefix, 'object', self, 'fieldname', 'melt_supply', 'format', 'DoubleMat', 'mattype', 1, 'timeserieslength', md.mesh.numberofvertices + 1, 'yts', md.constants.yts)
     # }}}
